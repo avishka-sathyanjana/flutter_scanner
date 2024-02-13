6f32769ed20f09f6fca478989bb6830b7352619a
shanuDhanusha <2020cs186@stu.ucsc.cmb.ac.lk>
2024-02-13 14:41:08 +0530

    fix:make admin dash bord

--- lib/screen/admin/admin_dash_bord.dart
+++ lib/screen/admin/admin_dash_bord.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../../data_validations/dilog_massage.dart';
+import '../../widget/drawer.dart';
 import '/style_varible/style_screen.dart';
+import '/widget/grid_card.dart';
 
 class AdminDsh extends StatefulWidget {
   const AdminDsh({super.key});
@@ -10,22 +14,58 @@ class AdminDsh extends StatefulWidget {
 
 class _AdminDshState extends State<AdminDsh> {
 
+  final GlobalKey<ScaffoldState> _scaffoldState=GlobalKey<ScaffoldState>();
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: colorPlate2,
-           title: const Text(
-             'Admin Dash Bord',
-             style: TextStyle(
-               color: Colors.white
+    List<Widget>card=[
+        GridCard(
+            userDifingFunction:(){
+
+            },
+            imageUrl: "assets/images/add-user.png",
+            funcName:"Create user"
+        )
+    ];
+    return WillPopScope(
+      onWillPop: ()async{
+      await showConfirmationDialog(context, "Exit","Do you want to exit !");
+      if(dilogState){
+        //AuthService().logOut();
+        SystemNavigator.pop();
+        dilogState=false;
+      }
+      return dilogState;
+    },
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: colorPlate2,
+             title: const Text(
+               'Admin Dash Bord',
+               style: TextStyle(
+                 color: Colors.white
+               ),
              ),
-           ),
+            leading: IconButton(
+              icon: const Icon(Icons.drag_indicator_outlined),
+              onPressed: (){
+                _scaffoldState.currentState?.openDrawer();
+              },
+            ),
+          ),
+        drawer: DrawerScreen(),
+        body:Container(
+            margin: const EdgeInsets.only(top: 20),
+            padding: const EdgeInsets.all(10),
+            child: ListView.builder(
+                itemCount: card.length,
+                itemBuilder: (context,int index){
+                  return card[index];
+                })
         ),
 
-      body: null,
-
+      ),
     );
   }
 }
